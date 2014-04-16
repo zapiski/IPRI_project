395075e7ad365862ab1f1c820584ec32c61ee532
dockiler <dk.nexus@gmail.com>
2014-04-16 14:30:03 +0200

    Rest

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,22 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngResource'])
+.factory('Class', ['$resource',
+  function($resource){
+    return $resource('JSON/classes.json', {}, {
+      query: {method:'GET', isArray:true}
+    });
+  }])
+.factory('Note', ['$resource',
+  function($resource){
+    return $resource('JSON/notes.json', {}, {
+      query: {method:'GET', isArray:true}
+    });
+  }])
+.factory('User', ['$resource',
+  function($resource){
+    return $resource('JSON/users.json', {}, {
+      query: {method:'GET', isArray:true}
+    });
+  }]);
