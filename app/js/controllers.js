e3b1ee44213169b6ff1b473865236561de9457ad
Nejc Vesel <nejc.vesel@gmail.com>
2014-04-17 13:33:49 +0200

    Dodal filtre

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,9 +5,12 @@
 angular.module('myApp.controllers', [])
   .controller('main', function($scope, Class) {
 	$scope.classes = Class.query();
+	$scope.orderProp = 'name';
   })
   .controller('browse', function($scope, Note) {
 	$scope.notes = Note.query();
+	$scope.orderProp = 'name';
+
   })
   .controller('user', function($scope, User) {
 	$scope.users = User.query();
