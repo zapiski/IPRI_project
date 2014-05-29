002a9d70a7c8f382c054cd3eefec5317a7885285
Rok Kolesa <rok.kolesa@gmail.com>
2014-05-30 00:43:52 +0200

    file upload rev1

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ var app = express();
 
 app.use(bodyParser());
 
-app.post('/file', function(req, res) {
+app.post('/file/:filename', function(req, res) {
   if(req.body.hasOwnProperty('data') || req.body.hasOwnProperty('file') || req.body.hasOwnProperty('url')|| req.hasOwnProperty('name')){
     res.statusCode = 400;
     return res.send('Error 400: Post syntax incorrect.');
   } 
+  req.pipe(fs.createWriteStream("app/files/" + req.params.filename));
   fs.writeFile("app/files/" + req.body.name, req.body.file, function(err) {
     if(err) {
         console.log(err);
     } else {
-        console.log("The file was saved!");
+        console.log(req.body + req.body.name + req.body.file);
     }
 }); 
   res.json(req.body.name);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -128,49 +128,42 @@ angular.module('myApp.controllers', [])
 
 			$scope.uploadNote = function()
 			{
-			var note = {
+				var note = {
 					"name": $scope.newnote.name,
 					"description": $scope.newnote.description,
 					"year": $scope.newnote.year,
 					"noteURL": "files/"+ $('#fileInfo').val(),
 				}
-				console.log("haha")
+				for (var i = 0; i < $scope.files.length; i++) {
+				  var file = $scope.files[i];
+				  $scope.upload = $upload.upload({
+					url: 'file/'+$('#fileInfo').val(), //upload.php script, node.js route, or servlet url
+					method: 'POST',
+					// method: 'POST' or 'PUT',
+					// headers: {'header-key': 'header-value'},
+					// withCredentials: true,
+					'file': file, // or list of files: $files for html5 only
+					/* set the file formData name ('Content-Desposition'). Default is 'file' */
+					//fileFormDataName: myFile, //or a list of names for multiple files (html5).
+					/* customize how data is added to formData. See #40#issuecomment-28612000 for sample code */
+					//formDataAppender: function(formData, key, val){}
+				  }).progress(function(evt) {
+					 console.log(file);
+					console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+				  }).success(function(data, status, headers, config) {
+					// file is uploaded successfully
+					console.log(data);
+				  });
+				  //.error(...)
+				  //.then(success, error, progress); 
+				  //.xhr(function(xhr){xhr.upload.addEventListener(...)})// access and attach any event listener to XMLHttpRequest.
+				}
 				$scope.noteLocation.$add(note);
-				$window.location.href = '#/uploadNote/';
-
+				$window.location.href = '#/main/';
 			}
 
 			$scope.onFileSelect = function($files) {
-			//$files: an array of files selected, each file has name, size, and type.
-			for (var i = 0; i < $files.length; i++) {
-			  var file = $files[i];
-			  $scope.upload = $upload.upload({
-				url: 'file', //upload.php script, node.js route, or servlet url
-				method: 'POST',
-				// method: 'POST' or 'PUT',
-				// headers: {'header-key': 'header-value'},
-				// withCredentials: true,
-				data: {myObj: $scope.myModelObj},
-				'file': file, // or list of files: $files for html5 only
-				/* set the file formData name ('Content-Desposition'). Default is 'file' */
-				//fileFormDataName: myFile, //or a list of names for multiple files (html5).
-				/* customize how data is added to formData. See #40#issuecomment-28612000 for sample code */
-				//formDataAppender: function(formData, key, val){}
-			  }).progress(function(evt) {
-				 console.log(file);
-				console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-			  }).success(function(data, status, headers, config) {
-				// file is uploaded successfully
-				console.log(data);
-			  });
-			  //.error(...)
-			  //.then(success, error, progress); 
-			  //.xhr(function(xhr){xhr.upload.addEventListener(...)})// access and attach any event listener to XMLHttpRequest.
-			}
-			/* alternative way of uploading, send the file binary with the file's content-type.
-			   Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
-			   It could also be used to monitor the progress of a normal http post/put request with large data*/
-			// $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
+				$scope.files = $files;
 		  };
 
 				
